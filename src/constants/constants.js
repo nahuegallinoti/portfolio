d49897720504d589e0e7b03f96ba92fa9c9891c9
Nahuel Gallinoti <nahuel.gallinoti@incluit.com>
2022-08-01 17:29:18 -0300

    update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,6 @@ export const projects = [
     title: "NFT Marketplace",
     description:
       "Marketplace to mint, buy, sell and stake NFT's using custom token (LUNGO Token)",
-    image: "/images/1.png",
     tags: ["Angular 13", "Truffle", "Ganache", "Solidity"],
     source: "https://github.com/nahuegallinoti/lungo-token-nft",
     visit: "https://google.com",
@@ -13,7 +12,6 @@ export const projects = [
     title: "Booking System",
     description:
       "Booking system for hotel, reservation management and tracking of consumption during the stay",
-    image: "/images/2.png",
     tags: ["Angular 11", "Angular Material", "Firebase"],
     source: "https://github.com/nahuegallinoti/reservas-app",
     visit: "https://tesis-a16ed.firebaseapp.com/",
@@ -23,29 +21,34 @@ export const projects = [
     title: "Booking Portal",
     description:
       "Portal for booking hotel rooms and services, with a simple and intuitive interface",
-    image: "/images/2.png",
     tags: ["Angular 11", "Angular Material", "Firebase"],
     source: "https://github.com/nahuegallinoti/portal-reservas",
     visit: "https://portal-reservas.web.app/",
     id: 2,
   },
   {
+    title: "Giphy App",
+    description: "Web application to search GIFs",
+    tags: ["React"],
+    source: "https://github.com/nahuegallinoti/giphy-react-app",
+    visit: "https://giphy-app-react-hooks.herokuapp.com/",
+    id: 3,
+  },
+  {
     title: "ChatApp",
     description: "Chat app",
-    image: "/images/4.jpg",
     tags: ["React", "Socket.io", "Node.js"],
     source: "https://github.com/nahuegallinoti/chatApp",
     visit: "https://react-socketio-express-chat.herokuapp.com/",
-    id: 3,
+    id: 4,
   },
   {
     title: "Portfolio",
     description: "Personal portfolio website",
-    image: "/images/4.jpg",
     tags: ["Next.js"],
     source: "https://github.com/nahuegallinoti/portfolio",
     visit: "https://portfolio-nahuel.herokuapp.com/",
-    id: 4,
+    id: 5,
   },
 ];
 
